38f9baee263e491177698f610a14de9b50ac5f77
herenow <leonardoshiro@gmail.com>
2014-10-11 03:18:24 -0300

    final sketch before v1

--- index.js
+++ index.js
@@ -1,77 +1,159 @@
+#!/usr/bin/env node
 // Entry point
 // Parse arguments and setup the enviroment
-var GameFile = require('./lib/Loaders/GameFile.js')
-var fs       = require('fs')
-var mkdirp = require('mkdirp')
 
-var extract_folder = './extract/'
+// Dependencies
+var argv = require('optimist')
+var Grf  = require('./lib/grf.js')
+var ProgressBar = require('progress')
+var Fs = require('fs')
 
-var file = {}
-file.fd = fs.openSync('./data.grf', 'r')
-var grf = new GameFile(file)
+// Setup args
+argv.options('g', {
+	alias: 'grf',
+	describe: 'The grf file to be worked on.',
+})
+argv.options('s', {
+	alias: 'search',
+	describe: 'Search a single file on the .grf or a list of files separated by comma. RegExp are supported.',
+})
+argv.options('l', {
+	alias: 'list',
+	describe: 'List files inside the grf.',
+})
+argv.options('o', {
+	alias: 'output',
+	describe: 'Output directoy to write the extracted files to.',
+})
+argv.options('e', {
+	alias: 'extract',
+	describe: 'Extract a single file from the .grf, prints to stdout. Example: grf-extractor -e data/clientinfo.xml > clientinfo.xml',
+})
+argv.options('c', {
+	alias: 'concurrency',
+	default: 100,
+	describe: 'Concurrency rate, how many parallel extractions should we do, set it to higher values for a faster extraction.',
+})
+argv.options('v', {
+	alias: 'verbose',
+	describe: 'Enable verbose output, will print debug messages.',
+})
+argv.options('h', {
+	alias: 'help',
+	describe: 'Print help and usage information',
+})
 
-var stack_max = 100
-var stack_size = 0
-var index = 0
 
-console.log('Extracting', grf.entries.length, 'files.')
+// Parse arguments
+var arg = argv.argv
 
-// Launch extractors
-while(stack_size < stack_max) {
-	stack_size++
-	extract_next()
+// Print help
+if(!arg.g || arg.h) {
+	console.error("Usage: grf-extractor -g data.grf -o output_dir")
+	argv.showHelp()
+	return
 }
 
-function extract_next() {
-	if(index > grf.entries.length) {
-		console.log('asd')
-		return
-	}
+// Init working grf
+var fd = Fs.openSync(arg.g, 'r')
 
-	var i = index++
+var grf = new Grf({
+	fd: fd
+})
 
-	var entry = grf.entries[i]
+// Set debugging mode
+if(typeof arg.d !== 'undefined') {
+	grf.printDebug = true
+}
 
-	var exists = grf.getFile(entry.filename, function(data) {
-		/*
-		var len = data.byteLength
-		var buf = new Buffer(len)
+// Search and print information about a single file in the grf 
+if(typeof arg.s === 'string') {
+	var list = arg.s.split(',')
 
-		for(var i = 0; i < len; i++) {
-			buf[i] = data[i]
-		}
-		*/
-		var buf = new Buffer( new Uint8Array(data) );
+	for(var i in list) {
+		var pattern = list[i]
 
-		var file = entry.filename.replace( /\\/g, '/'); // Replace \\ to /
-		var path = extract_folder + file.substring(0, file.lastIndexOf("/")); // Get only path without filename
-		var fullpath = extract_folder + file
-
-		mkdirp(path, function (err) {
-			if(err) {
-				console.log("Failed to mkdirp", path)
-				extract_next()
+		grf.searchPattern(new RegExp(pattern, 'gi'), function(matches) {
+			console.log("Search for %s:", pattern)
+			
+			if(! matches) {
+				console.log("- Nothing found matching this pattern.")
 				return
 			}
 
-			fs.writeFile(fullpath, buf, function (err) {
-				extract_next()
-				if (err) {
-					console.log(file, 'Failed to write extracted buffer to file')
-					return
-				}
-				console.log(file, 'extracted!');
-			});
-		});
-	})
+			for(var m in matches) {
+				var match = matches[m]
 
-	if(!exists) {
-		extract_next()
-		console.log(entry.filename, 'not found in grf?')
+				console.log("- File path: %s - File size: %d kb", match.filename, match.real_size/1000)
+			}
+		})
 	}
+
+	return
 }
 
-process.on('uncaughtException', function(err) {
-    // handle the error safely
-    console.log(err);
-});
+// List files inside the grf
+if(typeof arg.l !== 'undefined') {
+	var list = grf.entries
+
+	for(var i in list) {
+		var file = list[i]
+
+		console.log("%s - size in bytes: %d", file.filename, file.real_size)
+	}
+
+	return
+}
+
+// Extract a single file frm the grf
+if(typeof arg.e === 'string') {
+	var filename = arg.e
+
+	// We need to replace our normal slashes for windows double forward slash
+	// Since the .grf filenames are written as so. Example: data\\clientinfo.xl
+	filename = filename.replace( /\//g, '\\')
+
+	var exists = grf.getFile(filename, process.stdout.write)
+
+	if(exists === false) {
+		console.error("File not in the .grf, don't forget to put the data/ folder in front.'")
+		console.error("Example: -e data/clientinfo.xml")
+	}
+
+	return
+}
+
+// Default action, extract the entire grf
+var extraction = grf.extract({
+	output: (typeof arg.o !== 'string' ? '' : arg.o),
+	concurrency: arg.c,
+})
+
+extraction.on('start', function() {
+	console.log("Extraction has started on %d files.", grf.entries.length)
+	
+	// Pretty progress bar extraction on process
+	var bar = new ProgressBar('Progress [:current/:total] [:bar] :percent :elapseds - :etas', {
+		width: 100,
+		total: grf.entries.length
+	})
+	var timer = setInterval(function () {
+		var progress = extraction.progress()
+		
+		bar.tick(progress)
+
+		if (bar.complete) {
+			clearInterval(timer);
+		}
+	}, 100)
+})
+
+extraction.on('error', function(err) {
+	console.error("Extraction error", err)
+})
+
+// Profiler
+if(require('cluster').isMaster) {
+	var agent = require('webkit-devtools-agent');
+	agent.start()
+}
